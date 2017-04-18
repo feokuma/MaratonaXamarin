a4aad605dc949ddf111c79dd1805608af9e685b4
okuma <feokuma@hotmail.com>
2017-04-18 18:08:03 -0300

    Demo Android finished

--- MaratonaXamarin.AndroidApp/MainActivity.cs
+++ MaratonaXamarin.AndroidApp/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using MaratonaXamarin.Shared;
+using System;
+using System.Linq;
 
 namespace MaratonaXamarin.AndroidApp
 {
@@ -13,6 +16,25 @@ namespace MaratonaXamarin.AndroidApp
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+
+            var button = this.FindViewById<Button>(Resource.Id.btnCarregar);
+            var listview = this.FindViewById<ListView>(Resource.Id.lvwItems);
+
+            button.Click += async (sender, e) =>
+            {
+                var api = new UserApi();
+                var users = await api.ListAsync(new Developer
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Angelo",
+                    Email = "angelo@angelo.com",
+                    State = "SP",
+                    City = "Carapicuiba"
+                });
+
+                listview.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItemSingleChoice,
+                    users.OrderBy(y => y).Select(x => $"{x.Id} { x.Name}").ToArray());
+            };
         }
     }
 }
